be621256e9bb2ba75ef34fc8d69b180054f6a622
zooyaho <jiwoo1268@naver.com>
2024-08-30 16:02:56 +0900

    test: checkbox 상태에 따른 버튼 비활성화 테스트 코드 삭성하기

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -2,12 +2,29 @@ import { useState } from 'react';
 
 function App() {
   const [btnColor, setBtnColor] = useState('red');
+  const [btnIsDisabled, setBtnIsDisabled] = useState(false);
   const nextColor = btnColor === 'red' ? 'blue' : 'red';
+  const className = btnIsDisabled ? 'gray' : btnColor;
+
   return (
     <div>
-      <button className={btnColor} onClick={() => setBtnColor(nextColor)}>
+      <button
+        className={className}
+        onClick={() => setBtnColor(nextColor)}
+        disabled={btnIsDisabled}
+      >
         Change to {nextColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+        onChange={(e) => {
+          setBtnIsDisabled(e.target.checked);
+        }}
+      />
+      <label htmlFor="disable-button-checkbox">disable button</label>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,6 +1,7 @@
 import { logRoles } from '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { expect } from 'vitest';
 
 test('button flow check', () => {
   const { container } = render(<App />);
@@ -11,4 +12,24 @@ test('button flow check', () => {
   expect(btnEl).toHaveTextContent(/red/i); // red 문구 확인
   expect(btnEl).toHaveClass('blue'); // class 확인
   // expect(btnEl).toHaveStyle({ 'background-color': 'blue' });
+});
+
+/**
+ * flow: checkbox 상태에 따른 버튼 비활성화 상태 관리
+ */
+test('checkbox flow check', () => {
+  render(<App />);
+  const btnEl = screen.getByRole('button', { name: /blue/i });
+  const checkBoxEl = screen.getByRole('checkbox', { name: /disable button/i });
+
+  // 초기 상태 확인
+  expect(btnEl).toBeEnabled();
+  expect(checkBoxEl).not.toBeChecked(); // 선택되지 않은 체크박스 상태
+
+  fireEvent.click(checkBoxEl);
+  expect(btnEl).toBeDisabled(); // 버튼 비활성화되었는지 체크
+  expect(btnEl).toHaveClass('gray'); // class 확인
+
+  fireEvent.click(checkBoxEl);
+  expect(btnEl).toBeEnabled();
 });
