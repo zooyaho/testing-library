31f432a826f455cf580aa45371ac6faf3a64c208
zooyaho <jiwoo1268@naver.com>
2024-08-30 15:32:13 +0900

    test: click 시뮬레이션 테스트 코드 추가

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 function App() {
+  const [btnColor, setBtnColor] = useState('red');
+  const nextColor = btnColor === 'red' ? 'blue' : 'red';
   return (
     <div>
-      <button className="red">Change to blue</button>
+      <button className={btnColor} onClick={() => setBtnColor(nextColor)}>
+        Change to {nextColor}
+      </button>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,10 +1,14 @@
 import { logRoles } from '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('button > 올바른 색상과 문구인지 체크', () => {
+test('button flow check', () => {
   const { container } = render(<App />);
-  logRoles(container);
-  const btnEl = screen.getByRole('button', { name: /blue/i });
-  expect(btnEl).toHaveClass('red');
+  // logRoles(container);
+  const btnEl = screen.getByRole('button', { name: /blue/i }); // 버튼 존재와 문구 확인
+  expect(btnEl).toHaveClass('red'); // class 확인
+  fireEvent.click(btnEl); // 클릭 시뮬레이션
+  expect(btnEl).toHaveTextContent(/red/i); // red 문구 확인
+  expect(btnEl).toHaveClass('blue'); // class 확인
+  // expect(btnEl).toHaveStyle({ 'background-color': 'blue' });
 });
