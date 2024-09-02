e5409ff3c426cb10d27f3c8dea88c874ac1d0deb
zooyaho <jiwoo1268@naver.com>
2024-09-02 15:24:19 +0900

    feat: 약관 라벨 hover 시 popover 활성화 테스트 코드 추가

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
 import { expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 /* 초기 상태 테스트 flow */
 test('Initial conditions', () => {
@@ -20,7 +21,8 @@ test('Initial conditions', () => {
 });
 
 /* checkbox 체크 시 버튼 활성화 상태 체크 테스트 */
-test('Checkbox disables button on first click and enables on second click', () => {
+test('Checkbox disables button on first click and enables on second click', async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
 
   const checkboxEl = screen.getByRole('checkbox', {
@@ -30,9 +32,33 @@ test('Checkbox disables button on first click and enables on second click', () =
     name: /confirm order/i,
   });
 
-  fireEvent.click(checkboxEl);
+  await user.click(checkboxEl);
   expect(confirmBtnEl).toBeEnabled();
 
-  fireEvent.click(checkboxEl);
+  await user.click(checkboxEl);
   expect(confirmBtnEl).toBeDisabled();
+});
+
+/* popover의 활성화/비활성화 flow 테스트 */
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // popover 초기 상태 비활성화
+  const nullPopoverEl = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  );
+  expect(nullPopoverEl).not.toBeInTheDocument();
+
+  // 체크박스 라벨 hover 시 popover 활성화
+  const checkboxLabelEl = screen.getByText(/terms and conditions/i);
+  await user.hover(checkboxLabelEl);
+  const popoverEl = screen.getByText(
+    /no ice cream will actually be delivered/i,
+  );
+  expect(popoverEl).toBeInTheDocument();
+
+  // mouse out 시 popover 비활성화
+  await user.unhover(checkboxLabelEl);
+  expect(popoverEl).not.toBeInTheDocument();
 });
